2c5ff645a92ee24a9d0a48f0aa83507f936dd5c7
kevin <kevin@membersourcemedia.com>
2011-07-05 22:28:05 -0700

    working login

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,22 @@
-class BootStrap {
+import com.kl.todo.Role
+import com.kl.todo.User
+import com.kl.todo.UserRole
 
+class BootStrap {
+	def springSecurityService
+	
     def init = { servletContext ->
-        
+        def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true)
+	    def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
+
+        def adminUser = User.findByUsername('admin') ?: new User(
+                username: 'admin',
+                password: springSecurityService.encodePassword('admin'),
+                enabled: true).save(failOnError: true)
+
+        if (!adminUser.authorities.contains(adminRole)) {
+            UserRole.create adminUser, adminRole
+        }
     }
     def destroy = {
     }
--- grails-app/controllers/com/kl/todo/HomeController.groovy
+++ grails-app/controllers/com/kl/todo/HomeController.groovy
@@ -1,9 +1,13 @@
 package com.kl.todo
 
+import grails.plugins.springsecurity.Secured
+
 class HomeController {
+    def springSecurityService
 
     @Secured(['IS_AUTHENTICATED_REMEMBERED'])
     def index = { 
-    
+        def user = User.get(springSecurityService.principal.id)
+        [username : user.username]
     }
 }
